db81ddeb3f4fed82327e8bf6fb203cbbf4055cc5
lazy_forever <2606090126@qq.com>
2025-05-18 15:00:59 +0800

    update admin

--- FrontEnd/apps/home/routes.py
+++ FrontEnd/apps/home/routes.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from apps.home import blueprint
 from flask import render_template, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
 
 
@@ -15,6 +15,32 @@ def index():
 
     return render_template('home/index.html', segment='index')
 
+
+@blueprint.route('/tables')
+@blueprint.route('/tables.html')
+@blueprint.route('/tables/')
+@blueprint.route('/tables.htm')
+@blueprint.route('/tables.html/')
+@login_required
+def admin_template():
+    try:
+        template = 'tables.html'
+        if current_user.username != 'admin':
+            return render_template('home/page-403.html'), 403
+
+        # Detect the current page
+        segment = get_segment(request)
+        
+
+        # Serve the file (if exists) from app/templates/home/admin.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+    
 
 @blueprint.route('/<template>')
 @login_required
