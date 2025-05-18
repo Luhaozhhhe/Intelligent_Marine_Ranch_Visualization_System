2275450cf29eafc0a4a335faf1c220e67f839844
KaiserTT <kaisertan015@gmail.com>
2025-05-18 17:06:31 +0800

    intelligence center(demo)

--- apps/it_center/__init__.py
+++ apps/it_center/__init__.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from flask import Blueprint
 
 blueprint = Blueprint(
-    'charts_blueprint',
+    'it_center_blueprint',
     __name__,
-    url_prefix=''
+    url_prefix='/it_center'
 )
